a8f9812a3fe62edc51bc47c8d02cc7681a10711d
cptlazy1 <o.culhaci@gmail.com>
2023-11-19 15:37:05 +0100

    Add security filter chain

--- src/main/java/com/example/retrogamejock/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/retrogamejock/configuration/SecurityConfiguration.java
@@ -2,7 +2,13 @@ package com.example.retrogamejock.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +35,11 @@ public class SecurityConfiguration {
                 .password(passwordEncoder
                         .encode("password")).roles("USER").build();
 
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder
+                        .encode("admin")).roles("USER", "ADMIN").build();
         manager.createUser(user);
+        manager.createUser(admin);
         return manager;
     }
 
@@ -37,9 +48,36 @@ public class SecurityConfiguration {
 //
 //    }
 //
-//    @Bean
-//    public SecurityFilterChain securityFilterChain() {
-//
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.httpBasic(Customizer.withDefaults()).authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers(HttpMethod.GET,
+                                "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,
+                                "/games/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET,
+                                "/game-systems/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST,
+                                "/users").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST,
+                                "/games/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST,
+                                "/game-systems/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT,
+                                "/users/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT,
+                                "/games/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT,
+                                "/game-systems/**").hasRole("USER")
+                        .anyRequest().denyAll())
+                        .sessionManagement(session -> session
+                                .sessionCreationPolicy(
+                                        SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 
 }
